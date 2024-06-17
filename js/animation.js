5146ec40a006de3898d702d13134a9a190e443ef
Vishal Kumar Ojha <143877699+vishalkumarojha@users.noreply.github.com>
2024-06-17 14:15:05 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Vishal Ojha","Student","Developer", "UI/UX Designer", "Technical Writer", "Video Editor"];
+const textArray = ["Vishal Ojha","Developer", "UI/UX Designer", "Technical Writer", "Video Editor"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
