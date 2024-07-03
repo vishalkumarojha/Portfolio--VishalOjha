6fc129f5b8fa2182829a0c170198cd4797f725e5
Vishal Kumar Ojha <143877699+vishalkumarojha@users.noreply.github.com>
2024-07-03 21:19:47 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hey, I am Vishal Ojha</span> Student at VIT Bhopal University , with CONVERT in UI/UX , Python, JavaScript, Java, C++,C and more. Adept in frameworks like React, Django .... Currently shaping my personal brand as a web developer, content writer and video editor. Excited about technology, innovation, and the endless possibilities they bring. Let's connect and explore the intersection of tech and creativity!"
+        "<span>Hey, I am Vishal Ojha\n\</span> Student at VIT Bhopal University , with CONVERT in UI/UX , Python, JavaScript, Java, C++,C and more. Adept in frameworks like React, Django .... Currently shaping my personal brand as a web developer, content writer and video editor. Excited about technology, innovation, and the endless possibilities they bring. Let's connect and explore the intersection of tech and creativity!"
       ],
       typeSpeed: 11,
       contentType: 'html'
