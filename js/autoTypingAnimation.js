f22d8fd1d09525dbc5ba60d5bb89f09c84a63221
Vishal Kumar Ojha <143877699+vishalkumarojha@users.noreply.github.com>
2024-07-03 22:14:26 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,9 +1,9 @@
 setTimeout(function() {
-    $('.about-info-inner').typed({
-      strings: [
-        "<span>Hey, I am Vishal Ojha</span> <br><br> As a 2nd-year student at VIT University, Bhopal, I am passionate about leveraging technology to create impactful solutions. With a strong foundation in UI/UX design, Python, JavaScript, Java, and C++, I specialize in developing dynamic and user-friendly applications. My expertise extends to the MERN stack, and I am proficient in frameworks such as React, Django & Flask <br><br> In addition to my technical skills, I am dedicated to shaping my personal brand as a versatile web and Android developer. I also have experience as a content writer and video editor, which enhances my ability to communicate complex ideas effectively.<br><br> I currently serve as the Event Management Lead at Notion Community VIT Bhopal and am a member of the technical team at E-Cell VIT Bhopal. Driven by a love for technology and innovation, I am excited about the endless possibilities they bring. I am always eager to learn, grow, and collaborate on projects that push the boundaries of what is possible <br><br> Let's connect and explore opportunities to create something remarkable together."
-      ],
-      typeSpeed: 11,
-      contentType: 'html'
-    });
-  }, 100);
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hey, I am Vishal Ojha</span><br><br>As a 2nd-year student at VIT University, Bhopal, I am passionate about leveraging technology to create impactful solutions. With a strong foundation in UI/UX design, Python, JavaScript, Java, and C++, I specialize in developing dynamic and user-friendly applications. My expertise extends to the MERN stack, and I am proficient in frameworks such as React, Django & Flask.<br><br>In addition to my technical skills, I am dedicated to shaping my personal brand as a versatile web and Android developer. I also have experience as a content writer and video editor, which enhances my ability to communicate complex ideas effectively.<br><br>I currently serve as the Event Management Lead at Notion Community VIT Bhopal and am a member of the technical team at E-Cell VIT Bhopal. Driven by a love for technology and innovation, I am excited about the endless possibilities they bring. I am always eager to learn, grow, and collaborate on projects that push the boundaries of what is possible.<br><br>Let's connect and explore opportunities to create something remarkable together."
+    ],
+    typeSpeed: 11,
+    contentType: 'html'
+  });
+}, 100);
