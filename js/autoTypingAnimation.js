cec4d8421765c9ff496f0c61573d01bf71770717
vishal kumar ojha <vishal.23bhi10067@vitbhopal.ac.in>
2024-09-12 00:26:10 +0530

    fixs the theame error

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,9 +1,10 @@
 setTimeout(function() {
-    $('.about-info-inner').typed({
-      strings: [
-        "<span>Hey, I am Vishal Ojha</span> Student at VIT Bhopal University , with expertise in UI/UX , Python, JavaScript, Java, C++, MERN stack and more. Adept in frameworks like React, Django, Flask, Next.js .... Currently shaping my personal brand as a web and android developer, content writer and video editor. Excited about technology, innovation, and the endless possibilities they bring. Let's connect and explore the intersection of tech and creativity!"
-      ],
-      typeSpeed: 11,
-      contentType: 'html'
-    });
-  }, 100);
+  $('.about-info-inner').typed({
+    strings: [
+      `<span>Hey, I am Vishal Ojha</span> Student at VIT Bhopal University , with expertise in UI/UX , Python, JavaScript, Java, C++, MERN stack and more. Adept in frameworks like React, Django, Flask, Next.js .... Currently shaping my personal brand as a web and android developer, content writer and video editor. Excited about technology, innovation, and the endless possibilities they bring. Let's connect and explore the intersection of tech and creativity!<br>
+      `
+    ],
+    typeSpeed: 11,
+    contentType: 'html'
+  });
+}, 100);
